0f6f45118c32848b5f296d8bc48d3301c44c1143
Oscar Zúñiga <oscar20azm@gmail.com>
2021-12-16 11:05:27 -0600

    Production build

---
+++ frontend/build/precache-manifest.91315d73487336f67dc223310a6cd76b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c8e22417941698db0cc80d7b4c3dc63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "532ca79dffc5f128d45d",
+    "url": "/static/css/main.95a964ca.chunk.css"
+  },
+  {
+    "revision": "bf380524fde4c6ad0cfa",
+    "url": "/static/js/2.f218d685.chunk.js"
+  },
+  {
+    "revision": "b78d06d995050d23ec508cb4ba84482c",
+    "url": "/static/js/2.f218d685.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "550caeb23d2614018feb",
+    "url": "/static/js/3.6b2f8703.chunk.js"
+  },
+  {
+    "revision": "532ca79dffc5f128d45d",
+    "url": "/static/js/main.dcd091b7.chunk.js"
+  },
+  {
+    "revision": "1ba2804081f0a37864dd",
+    "url": "/static/js/runtime-main.a9a8a53e.js"
+  }
+]);
